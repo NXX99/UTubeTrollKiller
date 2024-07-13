352cf8ab96c3281ce7ba2f4442dea98f9b4df20e
NXX99 <91738741+NXX99@users.noreply.github.com>
2024-07-13 01:37:31 -0500

    Add files via upload
    Signed-off-by: NXX99 <91738741+NXX99@users.noreply.github.com>

--- changelog.md
+++ changelog.md
@@ -4,6 +4,7 @@
 
 |Version|Update classification|Update Note|
 |:-----:|:-----:|:----------|
+|July 13th, 2024|Major|Made every troll group vulnerable to this script by adding a mode prompt and adding a nuke mode that ignores any anti-keywording, which can bump the channel count higher.|
 |May 16th, 2024|Major|Milestone 1 release: It terminates itself when it can't find any more channels, it asks for a rate, it gives more info about the channels being fetched, and it has an improved filter.|
 |May 8th, 2024|Patch|Made it stop blocking the AUTTP and gave an actual tutorial. Also separated the changelog from the readme. (All of the MD stuff was recommended by my Dad's friend)|
 |May 7th, 2024|Minor|Now saves the collected UTTP member name and ID lists to separate text files instead of needing to copy them directly from the terminal window.|
--- index.js
+++ index.js
@@ -12,19 +12,26 @@ const {Client} = require("youtubei");
 const fs = require("fs");
 const term = require("terminal-kit").terminal
 const youtube = new Client();
-
+var count = 0
+var mode = ""
+var query = ""
 var inc = 0
 var sec = 0
 var min = 0
 var hour = 0
-var fileCont = ""
+var fileCont0 = ""
 var fileCont1 = ""
+var fileCont2 = ""
 term.drawImage("images/uttklogo.png", {terminal: term, shrink: { width: term.height, height: term.height } })
 function txtDraw(){
   term("UU      UU     TTTTTTTTTTTT     TTTTTTTTTTTT    KK      KK \nUU      UU          TT               TT         KK    KK \nUU      UU          TT               TT         KK  KK \nUU      UU          TT               TT         KKKK \nUU      UU          TT               TT         KK  KK \nUU      UU          TT               TT         KK    KK \n  UUUUUU            TT               TT         KK      KK")
   console.log("\n")
 }
 txtDraw()
+
+
+
+
 setInterval(function(){
   sec++
   if(sec >= 60){
@@ -38,28 +45,30 @@ setInterval(function(){
 function terminate(c) {
   term.grabInput(false) ;
   setTimeout( function() { 
-    fs.writeFileSync("idlist.txt", fileCont);
-    fs.writeFileSync("namelist.txt", fileCont1);
+    fs.writeFileSync(`${mode}idlist.txt`, fileCont0);
+      fs.writeFileSync(`${mode}namelist.txt`, fileCont1);
+      fs.writeFileSync(`${mode}cmblist.txt`, fileCont2);
     if(c != 1){term 
        ("Before the script stopped, ")}
-    term(`${inc} channels have been fetched over the span of ${hour} hours, ${min} minutes, and ${sec} Seconds. The channel IDs have been saved to a text file.`);
+    term(`\n${count} channels have been fetched over the span of ${hour} hours, ${min} minutes, and ${sec} Seconds. The channel IDs have been saved to a text file.\n`);
     process.exitCode = c
     process.exit() } , 100 ) ; // Closes when Alt + Q or Ctrl + C is hit
 }
 
-async function main(rate){
-  const channels = await youtube.search('"UTTP" -AUTTP -Anti-UTTP -UTTPP', { type: "channel", continuation: "Eu0FEgRVVFRQGuQFRWdJUUFrZ1VnZ0VZVlVOcGMyazJjblJpTVY5VlptUjJNMDB5TWxRMlltVlJnZ0VZVlVObVdsWjFiWHBKVW05bGRVMUJUM280WmkxalltRkJnZ0VZVlVOTGVqWlFWekV5Y1d0U05rSmZOblV5WDJ4elpqSm5nZ0VZVlVOeE4zaEJhV2RwWWxOWk5razNjalJtVjB0bU1tOTNnZ0VZVlVOQlNrWk5abmxYZDI5SE0wNURWRzlmV0RselYyTkJnZ0VZVlVNNFdsRlljVkJGTVcxT1dFazJPRWRDTWxaWGJVdG5nZ0VZVlVOdVlsQmhjUzFtTkZCVU1rdFNjMGQyTmt4ck5WSlJnZ0VZVlVOcU5HWjFXbFIyYmxsMlpVRnlYMk5IUVROeVgyeG5nZ0VZVlVOVmJUaHNhVXRqYmpOU1pXeHpZeTFNVlVaWWNuSjNnZ0VZVlVOUmRHSnlRVmhDVFVSWWRrTmxTRE15ZERGRmJFbFJnZ0VZVlVOU1kwTk5YMlJMWDBGamNUWlNZelZ4WWxaRWJIUm5nZ0VZVlVNMGNHbGpSbE5RUnpScFNrc3hkVFp4Y21GVE9FNW5nZ0VZVlVNNGVERnNjMGxCT0VkZlJWbEJSa1JUTVZoUmFGTkJnZ0VZVlVONGNFOVJNVjlRUVMxVkxVMTFTR0ZqTVU1eVJGRlJnZ0VZVlVOTldIb3RRWEpSVlhSaWVuVlpUM0YwVDNoRlFUSm5nZ0VZVlVOdFpYTkxaa3MxVjBJMmNUaFVVVmw2TkVwU1RuTkJnZ0VZVlVOaGJGUkdUbFpCU1U5blZHTldOMEpCZFVjelJIRm5nZ0VZVlVNMFQxOUJOazFuVW5aMFkwNVhXbmxmUm1SYWIwdDNnZ0VZVlVNMVgxaE5ZbFJCVVVKMWIxTnRhek5PWkVJdGRITlJnZ0VZVlVOemN6VnlURVpET0VsTFlWOWlWMXBZWm1wS1lXOW5zZ0VHQ2dRSUZSQUMYgeDoGCILc2VhcmNoLWZlZWQ%3D"
+async function main(rate,query){
+  const channels = await youtube.search(query, { type: "channel", continuation: "Eu0FEgRVVFRQGuQFRWdJUUFrZ1VnZ0VZVlVOcGMyazJjblJpTVY5VlptUjJNMDB5TWxRMlltVlJnZ0VZVlVObVdsWjFiWHBKVW05bGRVMUJUM280WmkxalltRkJnZ0VZVlVOTGVqWlFWekV5Y1d0U05rSmZOblV5WDJ4elpqSm5nZ0VZVlVOeE4zaEJhV2RwWWxOWk5razNjalJtVjB0bU1tOTNnZ0VZVlVOQlNrWk5abmxYZDI5SE0wNURWRzlmV0RselYyTkJnZ0VZVlVNNFdsRlljVkJGTVcxT1dFazJPRWRDTWxaWGJVdG5nZ0VZVlVOdVlsQmhjUzFtTkZCVU1rdFNjMGQyTmt4ck5WSlJnZ0VZVlVOcU5HWjFXbFIyYmxsMlpVRnlYMk5IUVROeVgyeG5nZ0VZVlVOVmJUaHNhVXRqYmpOU1pXeHpZeTFNVlVaWWNuSjNnZ0VZVlVOUmRHSnlRVmhDVFVSWWRrTmxTRE15ZERGRmJFbFJnZ0VZVlVOU1kwTk5YMlJMWDBGamNUWlNZelZ4WWxaRWJIUm5nZ0VZVlVNMGNHbGpSbE5RUnpScFNrc3hkVFp4Y21GVE9FNW5nZ0VZVlVNNGVERnNjMGxCT0VkZlJWbEJSa1JUTVZoUmFGTkJnZ0VZVlVONGNFOVJNVjlRUVMxVkxVMTFTR0ZqTVU1eVJGRlJnZ0VZVlVOTldIb3RRWEpSVlhSaWVuVlpUM0YwVDNoRlFUSm5nZ0VZVlVOdFpYTkxaa3MxVjBJMmNUaFVVVmw2TkVwU1RuTkJnZ0VZVlVOaGJGUkdUbFpCU1U5blZHTldOMEpCZFVjelJIRm5nZ0VZVlVNMFQxOUJOazFuVW5aMFkwNVhXbmxmUm1SYWIwdDNnZ0VZVlVNMVgxaE5ZbFJCVVVKMWIxTnRhek5PWkVJdGRITlJnZ0VZVlVOemN6VnlURVpET0VsTFlWOWlWMXBZWm1wS1lXOW5zZ0VHQ2dRSUZSQUMYgeDoGCILc2VhcmNoLWZlZWQ%3D"
                                                               
   });
   var selch = channels.items[inc]
   let found = []
   term.grabInput(true)
-  var count = 0
+  count = 0
   function add(){
     count++
     found.push(selch.id)
-    fileCont = fileCont + `${selch.id} \n`
+    fileCont0 = fileCont0 + `${selch.id} \n`
     fileCont1 = fileCont1 + `${selch.name} \n`
+    fileCont2 = fileCont2 + `${selch.name} (${selch.id}) \n`
   }
   function increment(){
     term.moveTo(1, term.height - 1)
@@ -106,9 +115,8 @@ Increment to Channel Ratio: ${ratio * 100}%
   
   setInterval(increment, rate)
 }
-
 function ratePrompt(){
-  console.log("Set an interval, in milliseconds. Faster rates means its more likely to encounter the script stalling, due to getting duplicate entries, because of rate limits. \n \n \n \n \n ")
+  console.log("Set an interval, in milliseconds. Faster rates means its more likely to encounter the script stalling, due to getting duplicate entries, because of rate limits. \n ")
   term.inputField(function (error, input){
     if (typeof Number(input) != 'number'){
       console.log("Woops! You need to type an interval, in ms.")
@@ -117,7 +125,7 @@ function ratePrompt(){
       rate = input
       console.log("")
       try{
-        main(input)
+        main(input, query)
       }
         catch(error){
           console.log('The script has stopped due to the error below:')
@@ -127,7 +135,29 @@ function ratePrompt(){
     }
   })
 }
-try{setTimeout(ratePrompt, 1000)}
+function nukePrompt(){
+	console.log('Do you want to enable nuke mode to ignore Anti accounts? (y/N)')
+	term.inputField(function (error, input){
+	if (input != "y" && input != "Y"){
+		query = `"${mode}" -A${mode} -Anti-${mode}`
+	} else {
+		query = `${mode}`
+	}
+	ratePrompt()
+})
+}
+function modePrompt(){
+	console.log("Set a query (E.X.: VGCP.) Leave blank to hit UTTP")
+	term.inputField(function (error, input){
+	if (input == ""){
+	mode = 'UTTP'	
+	} else {
+	mode = input
+	}
+	nukePrompt()
+})
+}
+try{setTimeout(modePrompt, 1000)}
 catch(error){
   console.log(`The script has encountered an error, and can't continue. The error that killed the script is below: \n${error}`)
   terminate()
